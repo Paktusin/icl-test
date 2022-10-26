1f69ecb68889741b95a2095814eab4473d49e97a
Mikhail <mikhail.davlichin@kaspersky.com>
2022-10-26 14:53:12 +0300

    123

--- server/middleware/cors.js
+++ server/middleware/cors.js
@@ -1,14 +1,10 @@
 const cors = require("cors");
 const corsOptions = {
     origin: [
-      "capacitor://localhost",
-      "ionic://localhost",
-      "http://localhost",
-      "http://localhost:8080",
       "http://localhost:8100",
       "https://paktusin.github.io",
     ],
     optionsSuccessStatus: 200,
   };
 
-module.exports = cors(corsOptions);
+module.exports = cors();
